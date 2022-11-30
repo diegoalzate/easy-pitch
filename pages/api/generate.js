41ed167342cf33f45fb7fcb77db34cdfd061ed17
Diego Alzate <diego35.da@gmail.com>
2022-11-29 23:08:18 -0500

    hooked up open ai

---
+++ pages/api/generate.js
@@ -0,0 +1,54 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `Write me a pitch for a product with a hook question, description and conclusion, do this in a Gary Vee style. Please make sure the description goes depth.
+Product: `;
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  const secondPrompt = `
+    Take the product, hook, description, conclusion and write it in a conversational way with a Gary Vee style. Add quirky jokes.
+
+    ${basePromptOutput.text}
+
+    Conversational Version:
+    `;
+
+  console.log(`API: ${secondPrompt}`);
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 500,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  console.log(`${basePromptOutput.text}
+  Conversational version: ${secondPromptOutput.text}`)
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({
+    output: `${basePromptOutput.text} \n Conversational version:${secondPromptOutput.text}`,
+  });
+};
+
+export default generateAction;
